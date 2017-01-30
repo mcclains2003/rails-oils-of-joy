6f6b9ba5a2f1c7f87ff51c2b1a79fcc658050805
mcclains2003 <mcclains2003@gmail.com>
2017-01-30 11:33:02 -0600

    Set up Oil controller actions

--- app/controllers/oils_controller.rb
+++ app/controllers/oils_controller.rb
@@ -1,2 +1,49 @@
 class OilsController < ApplicationController
+  before_action :set_oil, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @oil = Oil.new
+  end
+
+  def create
+    @oil = Oil.new(oil_params)
+
+    if @oil.save
+      redirect_to oil_path(@oil)
+    else
+      redirect_to new_oil_path
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @oil.update(oil_params)
+      redirect_to oil_path(@oil)
+    else
+      redirect_to edit_oil_path(@oil)
+    end
+  end
+
+  def index
+    @oils = Oil.all
+  end
+
+  def destroy
+  end
+
+  private
+
+  def oil_params
+    params.require(:oils).permit(:name, :description)
+  end
+
+  def set_oil
+    @oil = Oil.find(param[:id])
+  end
+
 end
