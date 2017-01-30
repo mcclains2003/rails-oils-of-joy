93d1a9b9d98ab551000a7fb4cfcfc34fe2f78018
mcclains2003 <mcclains2003@gmail.com>
2017-01-30 10:52:30 -0600

    Created category controller actions and added controller resource routes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,40 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :destroy]
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+  end
+
+  def destroy
+    @category.delete
+
+    redirect_to categories_path
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'categories/name'
-
   resources :oils
   resources :products
+  resources :categories
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
