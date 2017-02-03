3e543fc9e8abf6a116025aa7ff156811a7e61b39
mcclains2003 <mcclains2003@gmail.com>
2017-02-03 10:47:30 -0600

    created uncoded actions for variants controller and then set_variant and variant_params private methods

--- app/controllers/variants_controller.rb
+++ app/controllers/variants_controller.rb
@@ -1,2 +1,35 @@
 class VariantsController < ApplicationController
+  before_action :set_variant, only: [:show, :edit, :update, :destroy]
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_variant
+    @variant = Variant.find(params[:id])
+  end
+
+  def variant_params
+    params.require(:variant).permit(:size)
+  end
+
 end
