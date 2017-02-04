81ccb46b47fdfe62fd531c0c48fbeffce37b6e15
mcclains2003 <mcclains2003@gmail.com>
2017-02-04 12:41:32 -0600

    Created all products controller actions, worked on the nested form for products, and created blank views for the other actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,49 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      redirect_to new_product_path
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      redirect_to edit_product_path(@product)
+    end
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  def destroy
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :category_id, oil_ids: [], variant_ids: [])
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
 end
