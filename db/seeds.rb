902ffa73a088bd22e7b8226226c7724b893da02d
mcclains2003 <mcclains2003@gmail.com>
2017-02-08 11:38:12 -0600

    Fixed a reoccuring seeding issue and added a few more lines on the product show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# 10.times do
-#   Item.create(
-#     title: Faker::Commerce.product_name,
-#     inventory: Faker::Number.number(2),
-#     price: Faker::Number.number(4)
-#   )
-#   Category.create(title: Faker::Commerce.department)
-
-# end
-
-# counter = 1
-# Item.all.each do |item|
-#   item.category_id = counter
-#   item.save
-#   counter += 1
-# end
-
-# User.create(
-#   email: "dak@prescott.com",
-#   password: "87654321",
-#   password_confirmation: "87654321"
-# )
-
 
 category = Category.create([
   { name: "Lotion" }, { name: "Scrub" }
@@ -46,6 +23,10 @@ product = Product.create([
   { name: "Happy Feet", description: "Makes feet happy", category_id: category.first.id }
   ])
 
-ProductOil.create(product_id: product, oil_id: oil.first)
+ProductOil.create([
+  { product_id: product.first.id, oil_id: oil.first.id }
+  ])
 
-ProductVariant.create(product_id: product, variant_id: variant.first, price: 8.00)
+ProductVariant.create([
+  { product_id: product.first.id, variant_id: variant.first.id, price: 8.00 }
+  ])
