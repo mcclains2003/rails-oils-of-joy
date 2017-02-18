9c657194c03d17053250908fc801683dc4c8c6e2
mcclains2003 <mcclains2003@gmail.com>
2017-02-18 10:29:08 -0600

    Updated the product model with a simple validation and updated the product show view to only show volume/cost that are greater than zero

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,12 +4,20 @@ class Product < ActiveRecord::Base
   has_many :product_oils
   has_many :oils, through: :product_oils
 
-  has_many :volume_costs
+  has_many :volume_costs, :dependent => :destroy
+
+  validates :name, uniqueness: true
 
   def volume_costs_attributes=(volume_costs_hashes)
     volume_costs_hashes.each do |i, attributes|
-      self.volume_costs.build(volume: attributes[:volume], cost: attributes[:cost])
+      unless attributes[:volume] == "0" && attributes[:cost] == "0"
+        self.volume_costs.build(attributes)
+      end
     end
+  end
+
+  def volumes_with_data
+    volume_costs.where('volume > ? AND cost > ?', 0, 0)
   end
 
 end
