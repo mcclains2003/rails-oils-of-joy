a9b0e1b866f454d5926213d9452e627eb59857fd
mcclains2003 <mcclains2003@gmail.com>
2017-02-18 10:32:27 -0600

    Added basic uniqueness validations to category, oil, and volume

--- app/models/oil.rb
+++ app/models/oil.rb
@@ -1,4 +1,6 @@
 class Oil < ActiveRecord::Base
   has_many :product_oils
   has_many :products, through: :product_oils
+
+  validates :name, uniqueness: true
 end
--- app/models/volume_cost.rb
+++ app/models/volume_cost.rb
@@ -1,2 +1,5 @@
 class VolumeCost < ActiveRecord::Base
+  belongs_to :product
+
+  validates :volume, :cost, numericality: true
 end
