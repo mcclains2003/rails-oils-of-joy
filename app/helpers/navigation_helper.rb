19758389e8b27d0b45b52f3ec864b003bb92d5d3
mcclains2003 <mcclains2003@gmail.com>
2017-02-24 12:10:04 -0600

    Created link helper method and all categories method

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,36 +1,11 @@
 module NavigationHelper
 
-  # def navigation_item(nav_name, nav_path)
-  #   content_tag(:li, class: "nav-item") do
-  #     content_tag(:a, class: "nav-link", href: "nav_path")
-  #   end
-  # end
+  def link_helper(link_title, path, class_name)
+    link_to link_title, path, class: class_name
+  end
 
-  
+  def all_categories
+    @categories ||= Category.all
+  end
 
 end
-
-
-
-    #   <li class="nav-item">
-    #     <a class="nav-link" href="#">Features</a>
-    #   </li>
-
-
-
-    #   def sidebar_item(title, options = {}, &block)
-    #   url = options.fetch(:url, '#')
-    #   identifier = options.fetch(:identifier, url)
-
-    #   content_tag(:li, class: class_for_sidebar_item(url, identifier, block.present?)) do
-    #     link = link_to(url, options.fetch(:link_options, {})) do
-    #       fa_icon(options[:icon], text: title)
-    #     end
-
-    #     if block.present?
-    #       link + capture_haml(&block)
-    #     else
-    #       link
-    #     end
-    #   end
-    # end
