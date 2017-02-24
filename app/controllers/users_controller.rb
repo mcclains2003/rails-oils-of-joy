0fa12f2bcc22e82b651a04620ce593c02a5726a4
mcclains2003 <mcclains2003@gmail.com>
2017-02-24 11:35:00 -0600

    Added email to user permitted params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    return redirect_to signup_path unless user.save
+    session[:user_id] = user.id
+    redirect_to root_path, notice: "You have successfully signed up!"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
