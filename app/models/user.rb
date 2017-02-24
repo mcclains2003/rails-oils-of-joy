d01f555bff5b7910f8dc624c8b126d131ecbd1b1
mcclains2003 <mcclains2003@gmail.com>
2017-02-24 13:52:24 -0600

    Created class method for the omniauth hash items, to create a user or find the user if already created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :email, uniqueness: true
+  validates :email, presence: true
+
+  def self.find_or_create_by_omniauth(fb_auth_hash)
+    where(email: fb_auth_hash["info"]["email"]).first_or_create do |user|
+      user.name = fb_auth_hash["info"]["name"]
+      user.password = SecureRandom.hex
+    end
+  end
+
 end
