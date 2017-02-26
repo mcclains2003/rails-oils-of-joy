f817e155ee75f6ce969779653c90511a2daf4975
mcclains2003 <mcclains2003@gmail.com>
2017-02-26 13:16:49 -0600

    Created 2 new methods in the nav helper one for the product category dropdown and the other for the auth links

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -8,4 +8,24 @@ module NavigationHelper
     @categories ||= Category.all
   end
 
+  def product_category_index_dropdown
+    content_tag(:div, :class => 'dropdown-menu') do 
+      all_categories.collect do |category|
+        content_tag(:a, category.name, {class: 'dropdown-item', href: "/categories/#{ category.id }/products"})
+      end.join.html_safe
+    end
+  end
+
+  def authentication_nav_link(name, path, method = :get)
+    content_tag(:li, class: 'nav-item') do 
+      link_to name, path, method: method, class: 'nav-link'
+    end
+  end
+
+  def authentication_nav_text(user)
+    content_tag(:li, "Hello, #{ user.name }", class: 'nav-text') do 
+      "Hello, #{ user.name }"
+    end
+  end
+
 end
