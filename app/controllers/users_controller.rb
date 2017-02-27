55ae33f32417e63e101762fb9f55d5a337586250
mcclains2003 <mcclains2003@gmail.com>
2017-02-27 10:05:24 -0600

    Made sure users on the sign in page got the error messages when validations were not met

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    return redirect_to signup_path unless user.save
-    session[:user_id] = user.id
+    @user = User.new(user_params)
+    return render :new unless @user.save
+    session[:user_id] = @user.id
     redirect_to root_path, notice: "You have successfully signed up!"
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, uniqueness: true
-  validates :email, presence: true
+  validates :name, :email, presence: true
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 
   def self.find_or_create_by_omniauth(fb_auth_hash)
     where(email: fb_auth_hash["info"]["email"]).first_or_create do |user|
