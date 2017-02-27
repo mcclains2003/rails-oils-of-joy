8a83dd760289a0516038fe7c9fb40aab7ee0e960
mcclains2003 <mcclains2003@gmail.com>
2017-02-27 11:47:57 -0600

    Added multiple validations to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :email, uniqueness: true
   validates :name, :email, presence: true
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\A\S+@.+\.\S+\z/,
+    message: "must contain an @ and a ." }
 
   def self.find_or_create_by_omniauth(fb_auth_hash)
     where(email: fb_auth_hash["info"]["email"]).first_or_create do |user|
