8443d46de662da0f29c15fa57da3cf9bb0cb92d5
mcclains2003 <mcclains2003@gmail.com>
2017-03-01 11:05:09 -0600

    Completed category model association and validation tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Category, type: :model do
 
   describe 'associations & validations' do 
     it { should have_many(:products) }
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
   end
 
 end
