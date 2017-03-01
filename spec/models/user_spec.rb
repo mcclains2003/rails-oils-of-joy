8f695cd8b6baa8f7756c27ce7c422164334d4d9a
mcclains2003 <mcclains2003@gmail.com>
2017-03-01 11:34:47 -0600

    Added basic association and validation model tests to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'associations & validations' do 
+    it { should have_secure_password }
+    it { should validate_confirmation_of(:password) }
+    it { should validate_presence_of(:password_confirmation) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should allow_value('shaun@shaun.com', 'shaun.mcclain@mcclain.com').for(:email) }
+    it { should_not allow_value('shaunmcclain.com', '@mcclain.com', 'shaun@michael.mcclain').for(:email) }
+  end
+
 end
