bddd1097e0831c1a8604fe8887788ccd5964bb72
mcclains2003 <mcclains2003@gmail.com>
2017-03-01 11:34:27 -0600

    Added basic association and validation model tests to product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe Product, type: :model do
 
   describe 'associations & validations' do 
     it { should belong_to(:category) }
-
     it { should have_many(:oils).through(:product_oils) }
-    it { should have_many(:variants).through(:product_variants) }
+    it { should have_many(:volume_costs) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_uniqueness_of(:name) }
   end
 
 end
