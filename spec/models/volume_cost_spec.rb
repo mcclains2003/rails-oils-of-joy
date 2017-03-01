3a8a67f0579943def56bf3d9bbd53a06ec4c6291
mcclains2003 <mcclains2003@gmail.com>
2017-03-01 11:35:21 -0600

    Added basic association and validation model tests to volume_cost

--- spec/models/volume_cost_spec.rb
+++ spec/models/volume_cost_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe VolumeCost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'associations & validations' do 
+    it { should belong_to(:product) }
+    it { should validate_presence_of(:cost) }
+    it { should validate_presence_of(:volume) }
+    it { should validate_numericality_of(:cost) }
+    it { should validate_numericality_of(:volume) }
+  end
+
 end
