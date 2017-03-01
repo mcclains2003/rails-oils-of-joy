7f160b2763d77b511cd2e82c46db9dec68992f77
mcclains2003 <mcclains2003@gmail.com>
2017-03-01 11:34:03 -0600

    Added basic association and validation model tests to oil

--- spec/models/oil_spec.rb
+++ spec/models/oil_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Oil, type: :model do
 
   describe 'associations & validations' do 
     it { should have_many(:products).through(:product_oils) }
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
   end
 
 end
