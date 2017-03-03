cacc50431dfea1b1b67be5bb93e2e5368583b381
mcclains2003 <mcclains2003@gmail.com>
2017-03-03 11:39:29 -0600

    Added additional rspec validations for products and had to write a special one for oil ids and its special message

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,7 +8,14 @@ RSpec.describe Product, type: :model do
     it { should have_many(:volume_costs) }
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:category_id) }
     it { should validate_uniqueness_of(:name) }
+
+    it "is invalid without an oil selected" do 
+      product = Product.new(oil_ids: nil)
+      product.valid?
+      expect(product.errors[:oil_ids]).to include("must select at least one Oil")
+    end
   end
 
 end
