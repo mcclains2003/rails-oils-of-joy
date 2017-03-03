c6afb98b998ebfd680d5c04411907c8a92f95b8c
mcclains2003 <mcclains2003@gmail.com>
2017-03-03 12:06:06 -0600

    Attempting to update factory user to have a normal user and an admin

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,14 @@
 FactoryGirl.define do
   factory :user do
-    name "MyString"
-    password_digest "MyString"
+    name "Test Name"
+    sequence(:email) { |n| "email#{n}@test.com" }
+    password 'password'
+    password_confirmation 'password'
     admin false
   end
+
+  trait :admin_user do
+    admin true
+  end
+  
 end
