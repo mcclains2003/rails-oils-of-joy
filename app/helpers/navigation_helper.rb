468703c281fe10ea514bf37493971943d3559b0c
mcclains2003 <mcclains2003@gmail.com>
2017-05-30 12:15:53 -0500

    In the navigation helper I changed the product category dropdown method to include in the class a name of the category to be able to reach out using jquery

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -11,7 +11,9 @@ module NavigationHelper
   def product_category_index_dropdown
     content_tag(:div, :class => 'dropdown-menu') do 
       all_categories.collect do |category|
-        content_tag(:a, category.name, {class: 'dropdown-item', href: "/categories/#{ category.id }/products"})
+        classTitle = category.name.parameterize('_')
+
+        content_tag(:a, category.name, {class: "#{ classTitle } dropdown-item", href: "/categories/#{ category.id }/products"})
       end.join.html_safe
     end
   end
