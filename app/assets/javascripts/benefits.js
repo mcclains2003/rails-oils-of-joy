97142b08bd451ea0f74d0108c21a6d728204b97d
mcclains2003 <mcclains2003@gmail.com>
2017-06-06 12:47:03 -0500

    Simplified the prototype method with the bare minimum, handlebars was not updating the page without a refresh

--- app/assets/javascripts/benefits.js
+++ app/assets/javascripts/benefits.js
@@ -1,59 +1,18 @@
-// function Benefit(attributes) {
-//   this.description = attributes.description;
-//   // this.id = attributes.id;
-// };
-
-Benefit.success = function(json) {
-  var benefit = new Benefit(json);
-  var benefitLi = benefit.renderLI();
-  var $ol = $("div.benefit ul")
-  $ol.append(benefitLi);
-}
-
-Benefit.error = function(response) {
-  console.log("You broke it" + response)
-}
-
-$(function() {
-  Benefit.templateSource = $("#benefit-template").html();
-  Benefit.template = Handlebars.compile(Benefit.templateSource);
-});
-
-Benefit.prototype.renderLI = function() {
-  return Benefit.template(this);
+function Benefit(attributes) {
+  this.description = attributes.description;
 };
 
-// $(function() {
-//   $("form#new_benefit").on("submit", function(e) {
-//     e.preventDefault();
-
-//     var $form = $(this); // returns jquery form object
-//     var action = $form.attr("action"); // returns the form action (post, patch)
-//     var params = $form.serialize(); // returns a serialized string of data, in this case auth token and the value of the input
-
-//     $.ajax({
-//       url: action,
-//       data: params,
-//       dataType: "json",
-//       method: "POST"
-//     })
-//     // .success(function(json) {
-//     //   $("#benefit_description").val("")
-//     //   var $ol = $("div.benefit ul");
-//     //   $ol.append(response);
-//     // })
-//     .success(Benefit.success)
-//     .error(Benefit.error)
-//   })
-// })
+Benefit.prototype.renderLI = function() {
+  return `<li>${this.description}</li>`
+};
 
 $(function() {
   $("form.new_benefit").on("submit", function(e) {
     e.preventDefault();
 
     var $form = $(this);
-    var action = $form.attr("action"); 
-    var params = $form.serialize(); 
+    var action = $form.attr("action");
+    var params = $form.serialize();
 
     $.ajax({
       method: "POST",
@@ -61,15 +20,18 @@ $(function() {
       data: params,
       dataType: "json"
     })
-    .success(Benefit.success)
-    // .success(function(json) {
-    //   $("#benefit_description").val("")
-    //   var $ol = $("div.benefit ul");
-    //   source = $("#benefit-template").html();
-    //   template = Handlebars.compile(source);
-      
-    //   $ol.append(template(json));
-    // })
-    .error(Benefit.error)
+    .success(successCallback)
+    .error(errorCallback)
   });
-}); 
+});
+
+var successCallback = function(json) {
+  var benefit = new Benefit(json);
+  var benefitLi = benefit.renderLI();
+  var $ol = $("div.benefit ul")
+  $ol.append(benefitLi);
+}
+
+var errorCallback = function(response) {
+  console.log("You broke it" + response)
+}
