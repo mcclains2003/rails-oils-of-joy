42ee416328211c97484c4c906cb4544e785a7f99
mcclains2003 <mcclains2003@gmail.com>
2017-06-07 14:34:03 -0500

    Added a next product button to the product show page partial, on click it will update the data, currently working on updating the edit and delete buttons if at all possible

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,1 +1,42 @@
+var currency = function(integer) {
+  return "$" + integer + "0";
+}
 
+var nextProductOils = function(data) {
+  var productOilsList = data["oils"];
+  var oils = "";
+
+  productOilsList.forEach(function(oil) {
+    oils += "<li>" + oil["name"] + "</li>"
+  });
+  
+  $("ul#productOils").html(oils);
+}
+
+var nextProductVolumeCost = function(data) {
+  var productVolumeCosts = data["volume_costs"];
+  var volumeCosts = "";
+
+  productVolumeCosts.forEach(function(vc) {
+    volumeCosts += "<li>" + vc.volume + " oz - " + currency(vc.cost) + "</li>"
+  });
+  
+  $("ul#productVolumeCost").html(volumeCosts);
+}
+
+$(function () {
+  $("a.next-product").on("click", function() {
+    var nextProductID = parseInt($(".next-product").attr("data-id")) + 1;
+    var nextURL = "/products/" + nextProductID;
+
+    $.get("/products/" + nextProductID + ".json", function(data) {
+      $("#productName").text(data["name"]);
+      $("#productName").attr("href", nextURL);
+      $("#productDescription").text(data["description"]);
+      nextProductOils(data);
+      nextProductVolumeCost(data);
+
+      $("a.next-product").attr("data-id", data["id"])
+    })
+  });
+});
