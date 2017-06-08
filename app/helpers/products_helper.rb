dc2c6a0b8e1d48bd14f19be6ce5e1712f1ae8eba
mcclains2003 <mcclains2003@gmail.com>
2017-06-08 09:56:47 -0500

    Created a helper method that creates the click link (and all the needed data) for the js next product code

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,3 +1,11 @@
 module ProductsHelper
 
+  def next_product_link(action, product_id)
+    if action === "show"
+      link_to "Next Product", "#", class: "next-product", data: { id: product_id }
+    end
+  end
+
+
+# <a href="#" class="next-product" data-id=params["id"]>Next Product</a>
 end
