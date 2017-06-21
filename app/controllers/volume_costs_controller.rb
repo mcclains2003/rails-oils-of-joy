6fa1187d5186e840a74cbad84da258c35c1e08fe
mcclains2003 <mcclains2003@gmail.com>
2017-06-21 11:50:21 -0500

    Created an index actin that also accepts json and updated the strong params

--- app/controllers/volume_costs_controller.rb
+++ app/controllers/volume_costs_controller.rb
@@ -2,7 +2,22 @@ class VolumeCostsController < ApplicationController
   before_action :authorize
 
   def new
-    render :layout => false
+  end
+
+  def index
+    @category = Category.find(params[:category_id])
+    @volume_costs = @category.volume_costs
+    
+    respond_to do |format|
+      format.html
+      format.json { render json: @volume_costs, :layout => false }
+    end
+  end
+
+  private
+
+  def volume_cost_params
+    params.require(:volume_cost).permit(:volume, :cost, :category_id)
   end
 
 end
