4459178cd4cc252519b909accecb24e3ca85ea42
mcclains2003 <mcclains2003@gmail.com>
2017-07-06 15:53:51 -0500

    Completed oil delete using ajax and comments added

--- app/assets/javascripts/oils.js
+++ app/assets/javascripts/oils.js
@@ -1,18 +1,29 @@
+/*
+  1. Add event listeners to your delete buttons on the oils page 
+  2. on successfull AJAX delete request (201 status) of the oil 
+       -> Make sure to unbind the event listener for the delete button taht was clicked
+      -> remove the item, edit and delete dynamically from the DOM 
+*/
+
+// Create an Oil constructor
 function Oil(oil) {
   this.id = oil.id
   this.name = oil.name
   this.description = oil.description
 };
 
-Oil.destroy = function(oil) {
+// "instantiates" an oil object and calls the destroy prototype function
+Oil.setAndDestroy = function(oil) {
   var oil = new Oil(oil);
   oil.destroy();
 }
 
+// gets the id from the just created oil object and runs .remove() which rids us of any listeners - no need for unbind() or off()
 Oil.prototype.destroy = function() {
   $("li#oil_" + this.id).remove();
 }
 
+// .on "click" will get the specific input that clicked, prevent default, and then run an ajax delete call, on success it will run functions above
 $(function() {
   $("input.delete-button").on("click", function(e) {
     e.preventDefault();
@@ -26,6 +37,8 @@ $(function() {
       dataType: "json",
       method: "DELETE"
     })
-    .success(Oil.destroy)
+    .success(function(resp) {
+      Oil.setAndDestroy(resp)
+    })
   });
 })
